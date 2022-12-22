b73b5cc491224625571116208ed2e1278ee959b6
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-12-22 11:18:50 +0900

    [update]CharaController完成(仮)

--- Assets/FukuMen/Scripts/CharaController.cs
+++ Assets/FukuMen/Scripts/CharaController.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Zenject;
 
 public class CharaController : MonoBehaviour
 {
+    [SerializeField]
+    CharaType _charaType;
+
     [SerializeField, Range(-5, 5)]
     private int _speedPower = 0;
+
+    [SerializeField]
+    private float _jumpPower = 0f;
+
+    [SerializeField]
+    private float _jumpInterval = 0f;
 
     [SerializeField]
     private bool _isGrounded = false;
@@ -14,14 +22,54 @@ public class CharaController : MonoBehaviour
     [SerializeField]
     private bool _isDamage = false;
 
-    [Inject]
     IJump _iJump;
+
+    Rigidbody _rb;
+
+    private void Start()
+    {
+        if (_charaType == CharaType.PLAYER)
+        {
+            _iJump = GetComponent<PlayerInput>();
+        }
+        else if (_charaType == CharaType.CPU)
+        {
+            _iJump= GetComponent<EnemyInput>();
+        }
+
+        _rb = GetComponent<Rigidbody>();
+    }
 
     private void Update()
     {
         if (_iJump.IsJump())
         {
-
+            _rb.AddForce(transform.up * _jumpPower, ForceMode.Impulse);
+            _isGrounded = true;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Grounded"))
+        {
+            StartCoroutine(JumpInterval());
+        }
+        if (other.gameObject.CompareTag("asteroid"))
+        {
+            _speedPower--;
+        }
+    }
+
+    IEnumerator JumpInterval()
+    {
+        yield return new WaitForSeconds(_jumpInterval);
+        _isGrounded = false;
+    }
+}
+
+enum CharaType
+{
+    PLAYER,
+    CPU,
 }
