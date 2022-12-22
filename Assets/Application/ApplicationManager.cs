e35a7885b17a0342642bfe86cb758ec2bfaca828
NagasawaYusei <curga3013@gmail.com>
2022-12-22 15:58:01 +0900

    [Update] ApplicationManagerを軽くいじった

--- Assets/Application/ApplicationManager.cs
+++ Assets/Application/ApplicationManager.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ApplicationManager : MonoBehaviour
+public class ApplicationManager
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static ApplicationManager Instance = new ApplicationManager();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    int _score;
+
+    public int Score => _score;
 }
