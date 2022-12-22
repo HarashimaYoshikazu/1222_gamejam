5a1611883183fab0cd1f7b2bbc730701fe6ad161
aoyama_saito <saitomarimo1229@gmail.com>
2022-12-22 17:16:22 +0900

    Enemyのジャンプ処理のバグを修正

--- Assets/FukuMen/Scripts/EnemyInput.cs
+++ Assets/FukuMen/Scripts/EnemyInput.cs
@@ -10,14 +10,27 @@ public class EnemyInput : IInput
     private const float _rayDistance = 7f;
     private Vector3 _pivot = new Vector3(0, 0, 0.5f);
 
+    private bool _enabled = false;
+    private float _jumpTimer = 0;
     public bool IsJump(GameObject go)
     {
+        if(_enabled == true)
+        {
+            _jumpTimer += Time.deltaTime;
+            if (_jumpTimer >= 2f )
+            {
+                _enabled = false;
+                _jumpTimer = 0;
+            }
+        }
+
         Vector3 origin = go.transform.position + _pivot;
         Vector3 forward = go.transform.position + Vector3.forward + _pivot;
 
         Debug.DrawLine(origin, forward);
         if (Physics.Raycast(origin, forward, _rayDistance, LayerMask.NameToLayer("Obstacle")))
         {
+            _enabled = true;
             return true;
         }
         return false;
